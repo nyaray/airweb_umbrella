32a33cc9c7e378b651c39cc6320ed542de9628bf
Emilio Nyaray <emilio@nyaray.com>
2018-06-09 02:00:00 +0200

    var: fix lexing of input lines

--- apps/airweb/lib/airweb/reader.ex
+++ apps/airweb/lib/airweb/reader.ex
@@ -24,12 +24,12 @@ defmodule Airweb.Reader do
 
     Logger.debug ["process_line clean:", inspect line]
 
-    with :ok                <- check_line_length(line),
-         :ok                <- check_line_format(line),
-         {unsafe_time, tag} <- lex_line(line),
-         time               <- cannonicalize_line_time(unsafe_time)
+    with :ok    <- check_line_length(line),
+         :ok    <- check_line_format(line),
+         tokens <- lex_line(line),
+         time   <- cannonicalize_line_time(tokens["time"])
     do
-      build_meta(line, time, tag, latest_tag)
+      build_meta(line, time, tokens, latest_tag)
     end
   end
 
@@ -45,54 +45,30 @@ defmodule Airweb.Reader do
   end
 
   defp lex_line(line) do
-    [time | maybe_tag] = split_line line
-    tag = compute_tag maybe_tag
-    {time, tag}
+    re = ~r/(?<chunk>^\w+ +)?((?<time>\d{2}:\d{2}(-\d{2}:\d{2})?)(,?\s*(?<tag>\S.*))?)$/iu
+    Regex.named_captures(re, line)
   end
-
-  defp split_line(line) do
-    line
-    |> String.split(",", [parts: 2])
-    |> Enum.map(&String.trim/1)
-  end
-
-  defp compute_tag([]), do: :no_tag
-  defp compute_tag([tag]), do: tag
 
   defp cannonicalize_line_time(line) do
     case String.split line, "-" do
-      [interval] -> cannonicalize_interval interval
-      r=[_s, _e] -> cannonicalize_range r
+      [interval] -> {:interval, interval}
+      r=[_start, _stop] -> {:range, r}
     end
   end
 
-  defp cannonicalize_interval(interval) do
-    {:interval, cannonicalize_time interval}
-  end
-
-  defp cannonicalize_range(range) do
-    {:range, (for lim <- range, do: cannonicalize_time lim)}
-  end
-
-  defp cannonicalize_time(time), do: String.replace(time, ~r/[^0-9:-]/, "")
-
-  defp build_meta(line, safe_time, line_tag, latest_tag) do
-    tag         = compute_tag(line_tag, latest_tag)
-    chunk_start = compute_chunk_start(line)
-    chunk_tag   = compute_chunk_tag(line, chunk_start)
-    {:ok, {safe_time, tag, chunk_start, chunk_tag}}
-  end
-
-  defp compute_tag(:no_tag, last_tag), do: last_tag
-  defp compute_tag(candidate, _last_tag), do: candidate
-
-  defp compute_chunk_start(<< c::utf8, _::binary >>), do: not c in [?\s, ?\t]
-
-  defp compute_chunk_tag(_line, false), do: :no_tag
-  defp compute_chunk_tag(line, true) do
-    line
-    |> String.split(["\s", "\t"], parts: 2, trim: true)
-    |> hd()
+  defp build_meta(line, time, tokens, latest_tag) do
+    tag =
+      case Map.get(tokens, "tag") do
+        "" -> latest_tag
+        tag -> tag |> String.trim
+      end
+    chunk_start = line =~ ~r/^\S/iu
+    chunk_tag =
+      case Map.get(tokens, "chunk") do
+        ""        -> :no_tag
+        chunk_tag -> chunk_tag |> String.trim
+      end
+    {:ok, {time, tag, chunk_start, chunk_tag}}
   end
 
 end
